295a4b81a5c0c28347da0d744f08af2a7464e06c
ronald vilorio <soccer23415@gmail.com>
2018-04-07 21:25:52 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,17 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
+<<<<<<< HEAD
   def artist_name
     self.artist.name
   end
 
 
+=======
+  private
+    def song_count
+
+
+    end
+>>>>>>> 0835e8eab96cfce44e7f53115f49eaca83ba710b
 end
